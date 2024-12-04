7dbe3cb98962ab18d6605057cb050126ea5c7d8a
abdulkhaliq anwar <abdulkhaliqanwar@student,moringaschool.com>
2024-12-04 14:52:09 +0300

    phase-0intro-to-js-2-looping-code-along assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names =["Guadalupe", "Ollie", "Aki"]
+let thankYouCards = []
+function writeCards(names ,surprise){
+    for (let i = 0; i < names.length; i++) {
+     thankYouCards.push (`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`);
+    }
+    return thankYouCards;
+}
+
+function countDown(startingNumber) {
+    while (startingNumber > 0) {
+        console.log(startingNumber);
+        startingNumber -= 1;
+        }
+        console.log(startingNumber);  
+}
+      
